7e480855b62a8c3e893336a74874bdac5e56bcc2
Mikael <mkd@dynaudio.com>
2016-11-03 19:19:10 +0100

    Another weird commit

---
+++ Sloeber/Prototype/.ino.cpp
@@ -0,0 +1,15 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2016-11-03 18:59:56
+
+#include "Arduino.h"
+#include "Main.h"
+void setup() ;
+void loop() ;
+
+#include "Prototype.ino"
+
+
+#endif
