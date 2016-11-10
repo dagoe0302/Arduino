a891cc89309da2ea9de7b25f6692a52263030225
Mikael <mkd@dynaudio.com>
2016-11-10 13:07:31 +0100

    cleanup + added debouncing.pdf

--- Sloeber/Prototype/.ino.cpp
+++ Sloeber/Prototype/.ino.cpp
@@ -2,14 +2,14 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2016-11-03 14:36:52
+//This file has been generated on 2016-11-04 14:48:46
 
 #include "Arduino.h"
 #include "Main.h"
 void setup() ;
 void loop() ;
 
-#include "TimerTest.ino"
+#include "Prototype.ino"
 
 
 #endif
--- Sloeber/TimerTest/Include/Main.h
+++
@@ -1,2 +0,0 @@
-extern void Main(void);
-extern Setup(void);
